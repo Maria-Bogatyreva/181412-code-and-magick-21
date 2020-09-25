793e8e5f12d0b1a1ca8a7a3a667f5957455060af
Maria-Bogatyreva <bogatyreva_maria@mail.ru>
2020-09-25 23:02:03 +0700

    Клонирование волшебника
    Создание карточки волшебника на основе шаблона

--- js/setup.js
+++ js/setup.js
@@ -6,15 +6,19 @@ document.querySelector('.setup-similar').classList.remove('hidden'); //Блок 
 
 var similarListElement = document.querySelector('.setup-similar-list') //Список, куда вставляем
 
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
-//Генерируем массив объектов
+//Генерируем массив объектов волшебников
 var wizards = [];
 
-var getWizards = function () {
+var generateWizards = function () {
   for (var i = 0; i < 4; i++ ) {
     wizards[i] = {
       name: names[Math.floor(Math.random() * names.length)] + ' ' + surnames[Math.floor(Math.random() * surnames.length)],
@@ -25,4 +29,18 @@ var getWizards = function () {
   return wizards;
 };
 
-wizards = getWizards();
+wizards = generateWizards();
+
+//Клонирование волшебника
+var getWizard = function(wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+
+
