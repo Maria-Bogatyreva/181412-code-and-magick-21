20d129f3b93a4215418a6327b9f849b34937ad44
Maria-Bogatyreva <bogatyreva_maria@mail.ru>
2020-10-04 09:15:50 +0700

    Валидация формы

--- js/setup.js
+++ js/setup.js
@@ -10,6 +10,7 @@ var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Крис�
 var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+const fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 const amountWizards = 4;
 
 //Функция для получения случайного элемента
@@ -116,9 +117,34 @@ inputName.addEventListener ('keydown', function (evt) {
     evt.stopPropagation();
   }
 });
+//ВАЛИДАЦИЯ ФОРМЫ
+inputName.addEventListener('invalid', function () {
+  if (inputName.validity.tooShort) {
+    inputName.setCustomValidity('Не менее 2х символов!');
+  } else if (inputName.validity.tooLong) {
+    inputName.setCustomValidity('Не более 25 символов!');
+  } else if (inputName.validity.valueMissing) {
+    inputName.setCustomValidity('Введи хоть что-нибудь!');
+  } else {
+    inputName.setCustomValidity('');
+  }
+});
 
 
+const MIN_NAME_LENGTH = 2;
+const MAX_NAME_LENGTH = 25;
 
+inputName.addEventListener('input', function (){
+  let valueLength = inputName.value.length;
 
+  if (valueLength < MIN_NAME_LENGTH) {
+    inputName.setCustomValidity('Ещё ' + (MIN_NAME_LENGTH - valueLength) +' симв.');
+  } else if (valueLength > MAX_NAME_LENGTH) {
+    inputName.setCustomValidity('Удалите лишние ' + (valueLength - MAX_NAME_LENGTH) +' симв.');
+  } else {
+    inputName.setCustomValidity('');
+  }
 
+  inputName.reportValidity();
+});
 
