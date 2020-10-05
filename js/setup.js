da327ae460be66462d7006bd4a2181b1c3b110e5
Maria-Bogatyreva <bogatyreva_maria@mail.ru>
2020-10-05 10:42:02 +0700

    Функция userDialog
    Оформила показ окна функцией userDialog. Добавила истинную const

--- js/setup.js
+++ js/setup.js
@@ -1,14 +1,17 @@
 'use strict';
-const userDialog = document.querySelector('.setup'); //Окно настроек пользователя
-userDialog.classList.remove('hidden');
-
-document.querySelector('.setup-similar').classList.remove('hidden'); //Блок "Похожие персонажи"
-
+const AMOUNT_WIZARDS = 4;
 const names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 const surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 const coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 const eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
-const amountWizards = 4;
+
+//Функция для показа окна
+const showUserDialog = function() {
+  const userDialog = document.querySelector('.setup'); //Окно настроек пользователя
+  userDialog.classList.remove('hidden');
+
+  document.querySelector('.setup-similar').classList.remove('hidden'); //Блок "Похожие персонажи"
+}
 
 //Функция для получения случайного элемента
 const getRandomElement = function (elements) {
@@ -58,6 +61,7 @@ const addWizards = function () {
 similarListElement.appendChild(fragment);
 }
 
+showUserDialog();
 addWizards();
 
 
