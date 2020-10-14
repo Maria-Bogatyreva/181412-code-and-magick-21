6da9d553f75342c4b3b147dc050d980bc351df4d
Maria-Bogatyreva <bogatyreva_maria@mail.ru>
2020-10-14 19:56:46 +0700

    Функция для загрузки волшебников с сервера

--- js/backend.js
+++ js/backend.js
@@ -2,21 +2,64 @@
 (function () {
   const URL_SAVE = 'https://21.javascript.pages.academy/code-and-magick';
   const URL_LOAD = 'https://21.javascript.pages.academy/code-and-magick/data';
+  const StatusCode = {
+    OK: 200
+  };
+  const TIMEOUT_IN_MS = 100000;
 
   // Функция для ОТПРАВКИ данных на сервер
-  const save = function (data, onSuccess) {
+  const save = function (data, onLoad, onError) {
     const xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      onSuccess(xhr.response); //В случае успешной загрузки отдать содержимое ответа
+      if (xhr.status === StatusCode.OK) {
+      onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
     });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = TIMEOUT_IN_MS;
 
     xhr.open('POST', URL_SAVE) // хотим ОТПРАВИТЬ данные на этот URL
     xhr.send(data); // Отправляем данные!
-
   };
 
+// Функция для ЗАГРУЗКИ данных с сервера
+  const load = function (onLoad, onError) {
+    const xhr = new XMLHttpRequest;
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCode.OK) {
+      onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = TIMEOUT_IN_MS;
+
+    xhr.open('GET', URL_LOAD);
+    xhr.send(); // Получаем данные!
+  }
 
   window.backend = {
     save: save,
---
+++ js/modal.js
@@ -0,0 +1,66 @@
+'use strict';
+(function () {
+  //  Импортируемые данные
+  const userDialog = window.constant.userDialog;
+  const save = window.backend.save;
+  const load = window.backend.load;
+  const AMOUNT_WIZARDS = 4;
+
+  //  Клонирование карточки волшебника
+  const getWizard = function (wizard) {
+    const similarWizardTemplate = document.querySelector('#similar-wizard-template')
+        .content
+        .querySelector('.setup-similar-item');
+
+    const wizardElement = similarWizardTemplate.cloneNode(true);
+
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
+
+    return wizardElement;
+  };
+
+//  Функция, если ЗАГРУЗКА данных прошла успешно (добавление списка волшебников)
+  const loadHandler = function (wizards) {
+    const similarListElement = document.querySelector('.setup-similar-list');
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < AMOUNT_WIZARDS; i++) {
+      fragment.appendChild(getWizard(wizards[i]));
+    }
+    similarListElement.appendChild(fragment);
+
+    userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+// Функция, если что-то пошло не так (выводится сообщение об ошибке)
+  const errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+// Функция, если ОТПРАВКА данных прошла успешно
+  const saveHandler = function () {
+    userDialog.classList.add('hidden');
+  }
+
+
+// Добавление списка похожих волшебников
+load(loadHandler, errorHandler);
+
+// Отправляем данные формы на сервер
+  const form = userDialog.querySelector('.setup-wizard-form');
+
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    save(new FormData(form), saveHandler, errorHandler);
+  })
+})();
