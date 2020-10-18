943266e617ec8eb51b080eb844677323b2d7ed46
Maria-Bogatyreva <bogatyreva_maria@mail.ru>
2020-10-18 21:38:30 +0700

    Внесения кода по изменению цвета в модуль modal

--- js/modal.js
+++ js/modal.js
@@ -6,10 +6,44 @@
   const load = window.backend.load;
   const createErrorMessage = window.util.createErrorMessage;
   const addWizards = window.render.add;
+  const getRandomElement = window.util.getRandomElement;
 
   const AMOUNT_WIZARDS = 4;
+  const coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  const eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+  const fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
   let wizards = [];
+
+  const primaryWizard = document.querySelector('.setup-wizard');
+
+  // Изменение цвета мантии по клику
+  const wizardCoat = primaryWizard.querySelector('.wizard-coat');
+  const inputCoatColor = document.querySelector('input[name="coat-color"]');
+  wizardCoat.addEventListener('click', function () {
+    const color = getRandomElement(coatColors);
+    wizardCoat.style.fill = color;
+    inputCoatColor.value = color;
+  });
+
+  // Изменение цвета глаз по клику
+  const wizardEyes = primaryWizard.querySelector('.wizard-eyes');
+  const inputEyesColor = document.querySelector('input[name="eyes-color"]');
+  wizardEyes.addEventListener('click', function () {
+    const color = getRandomElement(eyesColors);
+    wizardEyes.style.fill = color;
+    inputEyesColor.value = color;
+  });
+
+  // Изменение цвета фаербола по клику
+  const wizardFireball = userDialog.querySelector('.setup-fireball-wrap');
+  const inputFireballColor = document.querySelector('input[name="fireball-color"]');
+  wizardFireball.addEventListener('click', function () {
+    const color = getRandomElement(fireballColors);
+    wizardFireball.style.backgroundColor = color;
+    inputFireballColor.value = color;
+  });
+
 
   const loadHandler = function (data) {
     wizards = data;
