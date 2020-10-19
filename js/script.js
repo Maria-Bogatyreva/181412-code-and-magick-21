528448248f2a2bde22d8899926315941af7b9878
Maria-Bogatyreva <bogatyreva_maria@mail.ru>
2020-10-19 11:41:00 +0700

    Разделение модулей настройки волшебника и отрисовки похожих

--- js/script.js
+++ js/script.js
@@ -8,11 +8,8 @@
   const addWizards = window.render.add;
   const getRandomElement = window.util.getRandomElement;
 
-  const AMOUNT_WIZARDS = 4;
-  const coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  const eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
-  const fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
+  //const AMOUNT_WIZARDS = 4;
   let coatColor = 'rgb(101, 137, 164)';
   let eyesColor ='black';
   let wizards = [];
@@ -51,37 +48,14 @@
     }));
   };
 
-  const primaryWizard = document.querySelector('.setup-wizard');
-
-  // Изменение цвета мантии по клику
-  const wizardCoat = primaryWizard.querySelector('.wizard-coat');
-  const inputCoatColor = document.querySelector('input[name="coat-color"]');
-  wizardCoat.addEventListener('click', function () {
-    const newColor = getRandomElement(coatColors);
-    wizardCoat.style.fill = newColor;
-    inputCoatColor.value = newColor;
-    coatColor = newColor;
+  window.wizard.setEyesChangeHandler(function (color) {
+    eyesColor = color;
     updateWizards();
   });
 
-  // Изменение цвета глаз по клику
-  const wizardEyes = primaryWizard.querySelector('.wizard-eyes');
-  const inputEyesColor = document.querySelector('input[name="eyes-color"]');
-  wizardEyes.addEventListener('click', function () {
-    const newColor = getRandomElement(eyesColors);
-    wizardEyes.style.fill = newColor;
-    inputEyesColor.value = newColor;
-    eyesColor = newColor;
+  window.wizard.setCoatChangeHandler(function (color) {
+    coatColor = color;
     updateWizards();
-  });
-
-  // Изменение цвета фаербола по клику
-  const wizardFireball = userDialog.querySelector('.setup-fireball-wrap');
-  const inputFireballColor = document.querySelector('input[name="fireball-color"]');
-  wizardFireball.addEventListener('click', function () {
-    const color = getRandomElement(fireballColors);
-    wizardFireball.style.backgroundColor = color;
-    inputFireballColor.value = color;
   });
 
 
