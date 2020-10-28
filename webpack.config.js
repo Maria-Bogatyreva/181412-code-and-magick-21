d02cba3724c77862fe38bea27c4b28ca1c04cd28
Maria-Bogatyreva <bogatyreva_maria@mail.ru>
2020-10-28 11:14:01 +0700

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+	entry: [
+		"./js/game.js",
+		"./js/constant.js",
+		"./js/util.js",
+		"./js/backend.js",
+		"./js/wizard.js",
+		"./js/render.js",
+		"./js/debounce.js",
+		"./js/script.js",
+		"./js/validate.js",
+		"./js/ialog.js",
+		"./js/move.js"
+	],
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname),
+		iife: true
+	},
+	devtool: false
+};
